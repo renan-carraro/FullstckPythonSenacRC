01d9363be3c5b6893ee02e28d8e7f6fa14fe84e8
Renan Carraro <carrarorenan0@gmail.com>
2025-03-12 19:45:34 -0300

    Add files via upload
    comandos ja revisados

--- atividade_10032025.js
+++ atividade_10032025.js
@@ -15,22 +15,22 @@ console.log(imc)
 
 let tem_experiencia = true;
 let idade = 22;
-let idade_maior = idade >=18 || tem_experiencia;
+let idade_maior = idade >=18 && tem_experiencia;
 console.log("tem experiencia", idade_maior);
 
 //Classificação de Níveis de Acesso Declare uma variável nivel_acesso com um valor entre 1 e 3. Use estruturas condicionais para imprimir:
 //"Acesso total" se o nível for 3. "Acesso parcial" se o nível for 2. "Acesso restrito" se o nível for 1
 
-let nivel_acesso = 3;
-if (nivel_acesso ===3){
+let nivel_acesso =3
+if (nivel_acesso === 3) {
     console.log("Acesso total");
-}else if (nivel_acesso <3){
+}else if (nivel_acesso <3) {
     console.log("Acesso parcial");
-} else if (nivel_acesso <2){
-    console.log("Acesso restrito");
-}else (bivel_acesso >3){
+}else if (nivel_acesso <2) { 
+    console.log("Acesso restrito")
+    }else { (nivel_acesso >3) 
     console.log("numero invalido");
-}
+}    
 
 
 //Conversão de Temperatura Declare uma variável celsius com um valor de temperatura em graus Celsius. Converta a temperatura para Fahrenheit usando a fórmula: F = (C * 9/5) + 32. Imprima o resultado.
@@ -39,14 +39,21 @@ let c = 94;
 let f =(c * 9/5) + 32;
 console.log("a temperatura é",f);
 
+//Verificação de Números Pares e Ímpares Declare uma variável numero com um valor inteiro. Use estruturas condicionais para verificar se o número é par ou ímpar e imprima o resultado.
+let numero = 7;
+if (numero % 2 === 0) {
+    console.log("O número é par");
+} else {
+    console.log("O número é ímpar");
+}
 
 //Cálculo de Frete com Base no Peso Declare uma variável peso com o peso de um pacote em kg. Calcule o frete com base nas seguintes regras
 
-let peso = 8;
+let peso2 = 8;
 let frete;
-if (peso <= 5) {
+if (peso2 <= 5) {
     frete = 20;
-} else if (peso > 5 && peso <= 10) {
+} else if (peso2 > 5 && peso2 <= 10) {
     frete = 35;
 } else {
         frete = 50; // Frete para pacotes acima de 10 kg
