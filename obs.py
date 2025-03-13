f1d6bd2b9147e0374b8ac0509d0527372853c318
Renan Carraro <carrarorenan0@gmail.com>
2025-03-12 21:43:49 -0300

    Add files via upload

---
+++ obs.py
@@ -0,0 +1,10 @@
+alunopython = "andrei"
+lista_alunospython= ["andrei","alyfer,amarndo,arthur"]
+for letra in lista_alunospython:
+    if letra == alunopython:
+        print("aluno matriculado.")
+    else:
+        print("aluno não matriculado.")
+
+for i in range (lista_alunospython):
+    print(lista_alunospython[i])
