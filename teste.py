0949669f95b78acd9afd98efcf200d393aac60a4
Renan Carraro <carrarorenan0@gmail.com>
2025-03-12 21:34:24 -0300

    teste de cod

---
+++ teste.py
@@ -0,0 +1,13 @@
+preco = float(input("digite seu peso: "))
+desconto =float(input("digite seu desconte: "))
+preco_final= preco-desconto
+print("preço:",preco_final )
+
+
+alunopython = "andrei"
+for i in alunopython:
+    print (i)
+
+
+    for i in range (1,6):
+        print (i)
