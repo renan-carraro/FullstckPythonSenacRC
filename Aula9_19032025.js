ee3c7146b952346bcc5e54ec6faeca15b3b4f2f2
crossns <carrarorenan0@gmail.com>
2025-03-19 21:40:15 -0300

    explicação do professor

---
+++ Aula9_19032025.js
@@ -0,0 +1,3 @@
+//vetores em javaScrip são estruturas de dados que permitem armazenar muitas variaveis
+let frutas = ["maçã", "banana", "laranja"];//pra vc chamar os itens começa por (0,1,2,3...)ou seja ser eu chamar apenas a banana vai seguir a ordem [maça=0,banana=1,laranja=2]
+console.log(frutas);// vai ficar console.log(frutas [1]) no termial vai aparecer "banana"
