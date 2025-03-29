8b71c7a75e1fd43b8d57ef2fe25ba297b9e9b210
Renan Carraro <carrarorenan0@gmail.com>
2025-03-28 21:38:36 -0300

    Delete atividade_py directory

--- atividade_py/atividade_5.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        c=float(input("temperatura em celsius: "))
-        f= (c * 9/2) +32
-        break
-    except ValueError:print("digite apenas numeros!")
-print("a temperatura em fahrenheit é: ", f)
