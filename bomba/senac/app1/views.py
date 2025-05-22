032743a842de248bf6d9dd442b0b39247584d5cf
Renan Carraro <carrarorenan0@gmail.com>
2025-05-21 21:33:40 -0300

    Delete bomba directory

--- bomba/senac/app1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def home(request):
-    return render(request,'home.html')
-def sobre(request):
-    return render(request,'sobre.html')
-def curso(request):
-    return render(request,'curso.html')
