7f792da5a9d23807348b308a22675c34b457cf01
Renan Carraro <carrarorenan0@gmail.com>
2025-05-21 21:34:08 -0300

    Delete projetosDjango directory

--- projetosDjango/academia/app01/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    return render(request,'home.html')
-def produtos(request):
-    return render (request,'produtos.html')
-def contatos(request):
-    return render(request,'contatos.html')
